f0b640a46a43614245755318154f24f0a3abd610
Simon N <simtuje@gmail.com>
2016-03-21 09:47:58 +0300

    added a class that contains miscellaneous utility methods

---
+++ src/main/java/com/sudoku/restservice/utils/SudokuUtils.java
@@ -0,0 +1,33 @@
+package com.sudoku.restservice.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * This class contains miscellaneous utility methods used within the Sudoku rest service project.
+ *
+ * @author Simon Njenga
+ * @since 0.1
+ */
+@Component("sudokuUtils")
+public class SudokuUtils {
+
+	/**
+     * Check if the input parameter contains only digit characters.
+     */
+    public boolean isDigit(String input) {
+		char[] characters = input.toCharArray();
+		for (int i = 0; i < characters.length; i++) {			
+			if (Character.isDigit(characters[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+    /**
+     * Convert the input parameter to Integer value.
+     */
+	public Integer toInteger(String param) {
+		return Integer.valueOf(param);
+	}
+}

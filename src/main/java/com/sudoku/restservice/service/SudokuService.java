60ee4080ae50c8a73c18792ac5df853282d94965
Simon N <simtuje@gmail.com>
2016-03-21 13:15:19 +0300

    update and typo to Javadocs

--- src/main/java/com/sudoku/restservice/service/SudokuService.java
+++ src/main/java/com/sudoku/restservice/service/SudokuService.java
@@ -5,8 +5,7 @@ import org.springframework.stereotype.Service;
 import com.sudoku.restservice.constants.SudokuStatus;
 
 /**
- * This class is the Sudoku service class that contains the logic to the Sudoku
- * puzzle.
+ * This class is the Sudoku service class that contains the logic to solving the Sudoku puzzle.
  * 
  * @author Simon Njenga
  * @since 0.1
@@ -15,7 +14,7 @@ import com.sudoku.restservice.constants.SudokuStatus;
 public class SudokuService {
 
     /**
-     *  Insert values on Sudoku puzzle
+     *  Insert values on the Sudoku puzzle
      */
 	public String insertValuesOnSudokuPuzzle(int row, int column, int value,
 			int[][] grid) {
@@ -23,13 +22,16 @@ public class SudokuService {
 		    return SudokuStatus.INVALID_INPUT;
 		} else {
 		    if (this.validateMovesOnSudokuPuzzle(row, column, value, grid).equals(SudokuStatus.VALID_MOVE)) {
+				// insert that valid value/move into the Sudoku puzzle				
 			    grid[row][column] = value;
-			    // if there are empty/free cells, the Sudoku is still not yet complete!
-			    if (this.getFreeCellList(grid) > 0) {
-				    return SudokuStatus.VALID_MOVE_SUDOKU_NOT_COMPLETE;
-			    } else {
-			    	// Sudoku is complete because there are no more empty/free cells
+				
+				//check if the Sudoku puzzle has been filled up with values
+			    if (this.getFreeCellList(grid) == 0) {
+				    // Sudoku puzzle is complete because there are no more empty/free cells
 				    return SudokuStatus.VALID_MOVE_SUDOKU_COMPLETE;
+			    } else {
+			    	// Sudoku puzzle is still not yet complete! because there are more empty/free cells
+					return SudokuStatus.VALID_MOVE_SUDOKU_NOT_COMPLETE;
 			    }	
 		    } else {
 			    return SudokuStatus.INVALID_MOVE_SUDOKU_NOT_COMPLETE;
@@ -38,7 +40,7 @@ public class SudokuService {
 	}
 
     /**
-     *  Validate moves on Sudoku puzzle
+     *  Validate moves on the Sudoku puzzle
      */
 	private String validateMovesOnSudokuPuzzle(int row, int column, int value,
 			int[][] grid) {
@@ -50,7 +52,7 @@ public class SudokuService {
 	}
 
     /**
-     * Obtain the number of free cells from the puzzle
+     * Obtain the number of free cells from the Sudoku puzzle
      */
 	private int getFreeCellList(int[][] grid) {
 		// Determine the number of free cells
@@ -63,7 +65,7 @@ public class SudokuService {
 				}
 			}
 		}
-		 // return a multi-dimensional array of free cells
+		 // return the number of free cells from the Sudoku puzzle
 		return numberOfFreeCells;
 	}
 
@@ -90,8 +92,7 @@ public class SudokuService {
 		// Check whether grid[i][j] is valid in the 3 by 3 box
 		for (int inRow = (row / 3) * 3; inRow < (row / 3) * 3 + 3; inRow++) {
 			for (int inColumn = (column / 3) * 3; inColumn < (column / 3) * 3 + 3; inColumn++) {
-				if (inRow != row && inColumn != column
-						&& grid[inRow][inColumn] == value) {
+				if (inRow != row && inColumn != column && grid[inRow][inColumn] == value) {
 					// The current value at grid[i][j] is invalid
 					return false;
 				}

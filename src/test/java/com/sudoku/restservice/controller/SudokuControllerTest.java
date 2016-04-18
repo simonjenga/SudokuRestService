b892bb5b227838d3487c48b46b7f147e2a483201
Simon N <simtuje@gmail.com>
2016-04-18 08:25:00 +0300

    change RequestMapping from GET to PUT

--- src/test/java/com/sudoku/restservice/controller/SudokuControllerTest.java
+++ src/test/java/com/sudoku/restservice/controller/SudokuControllerTest.java
@@ -133,7 +133,7 @@ public class SudokuControllerTest {
 	public void testControllerInsertInValidMoveInRow() throws Exception {
     	final String result = SudokuStatus.INVALID_MOVE_SUDOKU_NOT_COMPLETE;
     	
-    	this.mockMvc.perform(MockMvcRequestBuilders.get("/sudoku?row=1&column=1&value=8")
+    	this.mockMvc.perform(MockMvcRequestBuilders.put("/sudoku?row=1&column=1&value=8")
 			.contentType(MediaType.TEXT_PLAIN).content("{ }"))
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=ISO-8859-1"));

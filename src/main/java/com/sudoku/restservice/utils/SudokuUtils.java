8eda03d84bfa7fdbd2485b4789640cb4d0c27595
Simon N <simtuje@gmail.com>
2016-06-01 12:05:00 +0300

    unnecessary loop while checking for digit character

--- src/main/java/com/sudoku/restservice/utils/SudokuUtils.java
+++ src/main/java/com/sudoku/restservice/utils/SudokuUtils.java
@@ -12,22 +12,20 @@ import org.springframework.stereotype.Component;
 public class SudokuUtils {
 
     /**
-     * Check if the input parameter contains only digit characters.
+     * Check only the first character of the {@code String} input parameter on whether it is a digit.
+     * @param input the input parameter to be tested.
+     * @return Returns {@code true} if the character is a digit; {@code false} otherwise.
      */
     public boolean isDigit(String input) {
-        char[] characters = input.toCharArray();
-        for (int i = 0; i < characters.length; i++) {
-            if (Character.isDigit(characters[i])) {
-                return true;
-            }
-        }
-        return false;
+        return Character.isDigit(input.toCharArray()[0]);
     }
 
     /**
-     * Convert the input parameter to an integer value.
+     * Convert the supplied {@code String} input parameter into an {@code Integer} value.
+     * @param input the input parameter to be converted.
+     * @return Returns an {@code Integer} object holding the value of the specified {@code String}.
      */
-    public Integer toInteger(String param) {
-        return Integer.valueOf(param);
+    public Integer toInteger(String input) {
+        return Integer.valueOf(input);
     }
 }

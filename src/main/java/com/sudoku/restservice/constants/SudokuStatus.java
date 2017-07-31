6b10a1a6bf2d2230077cfbd0bb4402485a370cc3
Simon N <simtuje@gmail.com>
2017-07-31 12:20:00 +0300

    javadocs fix to class

--- src/main/java/com/sudoku/restservice/constants/SudokuStatus.java
+++ src/main/java/com/sudoku/restservice/constants/SudokuStatus.java
@@ -1,7 +1,8 @@
 package com.sudoku.restservice.constants;
 
 /**
- * This class contains constants for "STATUSes" used within the Sudoku rest service project.
+ * This class {@code SudokuStatus} contains several constants for various "STATUS" used within this
+ * Sudoku rest service project.
  *
  * @author Simon Njenga
  * @since 0.1
@@ -21,6 +22,16 @@ public class SudokuStatus {
     public static final String VALID_MOVE = "VALID_MOVE";
 
     /**
+     * Invalid move.
+     */
+    public static final String INVALID_MOVE = "INVALID_MOVE";
+
+    /**
+     * Invalid input.
+     */
+    public static final String INVALID_INPUT = "INVALID_INPUT";
+
+    /**
      * Valid move sudoku complete.
      */
     public static final String VALID_MOVE_SUDOKU_COMPLETE = "VALID_MOVE_SUDOKU_COMPLETE";
@@ -34,16 +45,6 @@ public class SudokuStatus {
      * Valid move sudoku not complete.
      */
     public static final String INVALID_MOVE_SUDOKU_NOT_COMPLETE = "INVALID_MOVE_SUDOKU_NOT_COMPLETE";
-
-    /**
-     * Invalid move.
-     */
-    public static final String INVALID_MOVE = "INVALID_MOVE";
-
-    /**
-     * Invalid input.
-     */
-    public static final String INVALID_INPUT = "INVALID_INPUT";
 
     /**
      * Only three arguments are allowed.

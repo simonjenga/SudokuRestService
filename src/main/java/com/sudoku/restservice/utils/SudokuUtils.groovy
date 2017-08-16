73169d837968dd4934872bcd62278c6808285bbe
Simon N <simtuje@gmail.com>
2017-08-16 09:35:00 +0300

    added class javadocs to groovy class

--- src/main/java/com/sudoku/restservice/utils/SudokuUtils.groovy
+++ src/main/java/com/sudoku/restservice/utils/SudokuUtils.groovy
@@ -1,5 +1,14 @@
-package com.sudoku.restservice.utils;
+package com.sudoku.restservice.utils
 
+import org.springframework.stereotype.Component
+
+/**
+ * This utility class {@code SudokuUtils} contains methods used within this project.
+ *
+ * @author Simon Njenga
+ * @since 0.1
+ */
+@Component("sudokuUtils")
 public class SudokuUtils {
 
 }

918b675f61580289d6e6287176e709f848969bf8
Simon N <simtuje@gmail.com>
2017-09-17 20:37:00 +0300

    conversion of class from java to groovy

---
+++ src/main/java/com/sudoku/restservice/constants/SudokuStatus.groovy
@@ -0,0 +1,12 @@
+package com.sudoku.restservice.constants
+
+/**
+ * This class {@code SudokuStatus} contains several constants for various "STATUS" used within this
+ * Sudoku rest service project.
+ *
+ * @author Simon Njenga
+ * @since 0.1
+ */
+public class SudokuStatus {
+
+}
